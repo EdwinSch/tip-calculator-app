f778c3df62fba7332dbe70dc3dbb669a55d02f9f
Edwin <el.schutjes@avans.nl>
2022-08-06 11:35:08 +0200

    added input error returns

--- script.js
+++ script.js
@@ -5,26 +5,35 @@ const tipAmountOutput = document.querySelector(".tipAmount");
 const totalAmountOutput = document.querySelector(".totalAmount");
 const resetBtn = document.getElementById("resetBtn")
 
+const billTotalInput = document.getElementById("billTotalInput");
+const noOfPersonsInput = document.getElementById("NoPersonsInput");
 
 // Function for each button
 tipBtns.forEach(button => {
     button.addEventListener('click', function () {
         // Get inputs
-        let billTotalInput = parseFloat(document.getElementById("billTotalInput").value);
-        let noOfPersonsInput = parseInt(document.getElementById("NoPersonsInput").value);
-        // Get percentage from btn
-        let percentageSelect = parseInt(this.dataset.perc)
-        // Sum 1 - tip per person
-        let totalTip = billTotalInput / 100 * percentageSelect
-        let tipPerPerson = totalTip / noOfPersonsInput;
-        // Push tip per person to DOM
-        tipAmountOutput.innerHTML = `&euro; ${tipPerPerson.toFixed(2)}`
-        // Sum 2 - total per person
-        let grandTotal = billTotalInput + totalTip;
-        let totalPerPerson = grandTotal / noOfPersonsInput;
-        // push total per person to DOM
-        totalAmountOutput.innerHTML = `&euro; ${totalPerPerson.toFixed(2)}`;
+        let billValue = parseFloat(billTotalInput.value);
+        let personsValue = parseInt(noOfPersonsInput.value);
 
+        if (isNaN(billValue)) {
+            inputError(billTotalInput);
+        }
+        if (isNaN(personsValue)) {
+            inputError(noOfPersonsInput);
+        } else {
+            // Get percentage from btn
+            let percentageSelect = parseInt(this.dataset.perc)
+            // Sum 1 - tip per person
+            let totalTip = billValue / 100 * percentageSelect
+            let tipPerPerson = totalTip / personsValue;
+            // Push tip per person to DOM
+            tipAmountOutput.innerHTML = `&euro; ${tipPerPerson.toFixed(2)}`
+            // Sum 2 - total per person
+            let grandTotal = billValue + totalTip;
+            let totalPerPerson = grandTotal / personsValue;
+            // push total per person to DOM
+            totalAmountOutput.innerHTML = `&euro; ${totalPerPerson.toFixed(2)}`;
+        }
 
     })
 });
@@ -34,20 +43,29 @@ tipBtns.forEach(button => {
 
 customPercentageInput.addEventListener('keyup', function () {
     // Get inputs
-    let billTotalInput = parseFloat(document.getElementById("billTotalInput").value);
-    let noOfPersonsInput = parseInt(document.getElementById("NoPersonsInput").value);
-    // Get percentage from input
-    let customPercentage = parseInt(customPercentageInput.value);
-    // Sum 1 - tip per person
-    let totalTip = billTotalInput / 100 * customPercentage;
-    let tipPerPerson = totalTip / noOfPersonsInput;
-    // Push tip per person to DOM
-    tipAmountOutput.innerHTML = `&euro; ${tipPerPerson.toFixed(2)}`
-    // Sum 2 - total per person
-    let grandTotal = billTotalInput + totalTip;
-    let totalPerPerson = grandTotal / noOfPersonsInput;
-    // push total per person to DOM
-    totalAmountOutput.innerHTML = `&euro; ${totalPerPerson.toFixed(2)}`;
+    let billValue = parseFloat(billTotalInput.value);
+    let personsValue = parseInt(noOfPersonsInput.value);
+
+    if (isNaN(billValue)) {
+        inputError(billTotalInput);
+    }
+    if (isNaN(personsValue)) {
+        inputError(noOfPersonsInput);
+    }
+    else {
+        // Get percentage from input
+        let customPercentage = parseInt(customPercentageInput.value);
+        // Sum 1 - tip per person
+        let totalTip = billValue / 100 * customPercentage;
+        let tipPerPerson = totalTip / personsValue;
+        // Push tip per person to DOM
+        tipAmountOutput.innerHTML = `&euro; ${tipPerPerson.toFixed(2)}`
+        // Sum 2 - total per person
+        let grandTotal = billValue + totalTip;
+        let totalPerPerson = grandTotal / personsValue;
+        // push total per person to DOM
+        totalAmountOutput.innerHTML = `&euro; ${totalPerPerson.toFixed(2)}`;
+    }
 
 })
 
@@ -58,5 +76,14 @@ resetBtn.addEventListener('click', function () {
 
 })
 
-// TO DO: Error No of people input when empty
+// GLOBAL functions 
+
+function inputError(inputfield) {
+    inputfield.style.background = "red";
+    setTimeout(() => {
+        inputfield.style.background = "#dbeaeb";
+    }, 1000);
+}
+
 // TO DO: REFACTOR Math sum
+
